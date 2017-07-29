53b3997b157f10f02dd9bc67b3fe8b057eb889a5
Robert David <rdavid@glensoutdoors.com>
2017-07-29 13:06:14 -0500

    Initial repo

---
+++ README.md
@@ -0,0 +1,95 @@
+# Dom Creator
+
+Simple JS script used to quickly create DOM Elements.
+
+## Usage
+
+Instead of doing the following:
+
+```js
+var div = document.createElement('div');
+var id  = div.createAttribute('id');
+var klass = div.createAttribute('class');
+
+id.value = 'some_identifier';
+klass.value = 'some-class'
+div.setAttributeNode(id);
+div.setAttributeNode(klass);
+div.innerHTML = 'The lazy cat is sleeping again!';
+
+```
+
+You can just do :
+
+```js
+var div = new domCreator({
+    type:  'div',
+    id:    'some_identifier',
+    class: 'success',
+    innerHtml: 'The lazy cat is sleeping again!',
+    bind:   {
+        event: 'click',
+        method: function(){
+            alert('Is he going to sleep all day?');
+        }
+    }
+});
+```
+You can use methods like <code>addAttributeToElement</code> to add more attributes later after constructor, or <code>addChildToElement</code> to pass in either an object of paramters or another instance of domCreator, with its own attributes and bindings already set.  Additional bindings can be added by leveraging the <code>bindEvent</code> method.
+
+```js
+div.addAttributeToElement('data-employee-id', 123456789);
+div.addChildToElement({
+    type:  'span',
+    class: 'bold',
+    innerHtml: 'Wake him up!',
+    bind: {
+        event: 'click',
+        method: wake_up_cat()
+    }
+});
+
+//or
+
+button = new domCreator({
+    type:  'span',
+    class: 'bold',
+    innerHtml: 'Wake him up!',
+});
+
+button.bindEvent('click', wake_up_cat());
+div.addChildToElement(button.toHtml());
+```
+
+The <code>toHtml</code> method returns the Element you have built and not an instance of domCreator.  Every method of domCreator returns the instance of itself.  When you are ready to append the Element, or inject it into another Element, use <code>toHtml</code> to get the newly created element.
+
+```js
+    var container = document.getElementById('page_wrapper');
+    container.appendChild(div.toHtml())
+```
+
+There is a built in helper for select boxes.  You can use teh above methods to build the select box and built all the child options but that gets a little long.  So the shortcut for select boxes and options is:
+
+```js
+var select = new domCreator({
+    type: 'select',
+    options: [
+        {
+            label: 'name',
+            value: 1
+        },
+        {
+            label: 'sex', value: '2'
+        }
+    ]
+});
+
+//or
+
+var select = new domCreator({ type: 'select'});
+select.buildSelectOptions(
+    [{ label: '', value: '' }, { label: '', value: '' }]
+)
+```  
+
+With the helper method, domCreator will check to see if the element type is a selectbox before adding the childs.  If it is not, it will just return the instance of itself.
---
+++ domCreator.js
@@ -0,0 +1,143 @@
+/**
+ * 
+ * @param {object} params
+ * 
+ * @keys
+ *  - type      required    {string}
+ *  - id        required    {string}
+ *  - style     required    {string}
+ *  - value     required    {string}
+ *  - innerHtml required    {string}
+ *  - options   optional    {array|object}
+ *      @keys
+ *       - value    optional    {string}
+ *       - label    optional    {string}
+ *  - bind      optional    {object}
+ *      @keys
+ *       - event    required    {string}
+ *       - method   required    {string|function}
+ *  
+ * @returns {undefined|domCreator}
+ */
+function domCreator(params){
+    if (typeof(params["type"]) === "undefined") return;
+    this.type    = typeof(params["type"])     !== "undefined" ? params["type"]      : false;
+    this.id      = typeof(params["id"])       !== "undefined" ? params["id"]        : false;
+    this.style   = typeof(params["style"])    !== "undefined" ? params["style"]     : false;
+    this.value   = typeof(params["value"])    !== "undefined" ? params["value"]     : false;
+    this.html    = typeof(params["innerHtml"])!== "undefined" ? params["innerHtml"] : false;
+    this.options = typeof(params["options"])  !== "undefined" ? params["options"]   : false;
+    this.bind    = typeof(params["bind"])     !== "undefined" ? params["bind"]      : false;
+    this.element = document.createElement(this.type);
+    
+    if (this.id)    this.addAttributeToElement("id", this.id);
+    if (this.style) this.addAttributeToElement("class", this.style);
+    if (this.value) this.addAttributeToElement("value", this.value);
+    if (this.html)  this.element.innerHTML = this.html;
+    if (this.bind)  this.bindEvent(this.bind["event"], this.bind["method"]);
+    
+    if (this.options) this.buildSelectOptions();
+    
+    return this;
+};
+/**
+ * @param {array} _options
+ * @returns {domCreator}
+ */
+domCreator.prototype.buildSelectOptions = function(_options){
+    if (this.type &&
+        this.type.toLowerCase() == 'select' &&
+        typeof(options) !== undefined)
+    {
+        var options = this.options || _options;
+
+        for (var option in options){
+            if (options.hasOwnProperty(option)){
+                this.addChildToElement({
+                    type        : "option",
+                    id          : null,
+                    style       : null,
+                    value       : options[option]["value"],
+                    innerHtml   : options[option]["label"],
+                    options     : null,
+                    bind        : null
+                });
+            }
+        }
+    }
+
+    return this;
+}
+/**
+ * 
+ * @param {string} type
+ * @param {string} value
+ * @returns {domCreator}
+ */
+domCreator.prototype.addAttributeToElement = function(type, value){
+    if (false === this.element instanceof HTMLElement) return this;
+    var attr = document.createAttribute(type);
+    attr.value = value;
+    this.element.setAttributeNode(attr);
+    return this;
+};
+/**
+ * 
+ * @param {object} object
+ * 
+ * keys
+ *  - type      required    {string}
+ *  - id        required    {string}
+ *  - style     required    {string}
+ *  - value     required    {string}
+ *  - innerHtml required    {string}
+ *  - options   optional    {array|object}
+ *      keys
+ *       - value    optional    {string}
+ *       - label    optional    {string}
+ *  - bind      optional    {object}
+ *      keys
+ *       - event    required    {string}
+ *       - method   required    {string|function}
+ *  
+ * @returns {undefined|domCreator}
+ */
+domCreator.prototype.addChildToElement = function(object){
+    if (false === this.element instanceof HTMLElement) return this;
+    if (true === object instanceof domCreator){
+        var childElem = object;
+    } else {
+        var childElem = new domCreator(object);    
+    }
+    
+    this.element.appendChild(childElem.toHtml());
+    return this;
+};
+/**
+ * 
+ * @returns {Element}
+ */
+domCreator.prototype.toHtml = function(){
+    if (this.element instanceof HTMLElement){
+        return this.element;
+    }
+};
+/**
+ * 
+ * @param {string} event
+ * @param {string|function} method
+ * @returns {domCreator}
+ */
+domCreator.prototype.bindEvent = function(event, method){
+    if (false === this.element instanceof HTMLElement) return this;
+
+    if (this.element.addEventListener){
+        this.element.addEventListener(event, method);
+
+    } else if (this.element.attachEvent){
+        this.element.attachEvent(event, method);
+
+    }
+
+    return this;
+};
